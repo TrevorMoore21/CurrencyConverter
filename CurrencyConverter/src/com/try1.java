661946f105f1ff360c2517f47973f926365210ea
trevmo303 <trevmo303@localhost>
2022-02-06 12:20:12 -0700

    idk what im doing

---
+++ CurrencyConverter/src/com/try1.java
@@ -0,0 +1,31 @@
+package com;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.io.InputStream;
+
+	
+		class try1
+		{
+		private String lhs;
+		private String rhs;
+		private String error;
+		private String icc;
+		public static void Recv()
+		{
+			
+		}
+		public String getLhs()
+		{
+		return lhs;
+		}
+		public String getRhs()
+		{
+		return rhs;
+		}
+		}
+		
+		
+		
+		
+	
